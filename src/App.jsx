21d3b4e50e4173eeea7b1206b892f80237b7d4b7
lenny dzatsi <lennydzatsi8@gmail.com>
2023-11-30 12:39:07 +0100

    mise a jours des pages et ajouts des données

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Products from "./pages/products"
+import About from "./pages/about"
+import ProductsList from "./pages/productsList"
+import SingleProduct from "./pages/singleproduct"
+import NavBar from "./components/navBar"
+
+
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>     
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Products" element={<Products />} >
+          <Route index element={<ProductsList />} />
+          <Route path=":id" element={<SingleProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

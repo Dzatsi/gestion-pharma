222324908f8ed4cc167dcb6b3e47377e41bfe1e8
lenny dzatsi <lennydzatsi8@gmail.com>
2023-12-01 12:13:22 +0100

    creattion du fichier productPreview, configuration des nouvelles routes, et usages des nouveaux hooks tels que useNavigate et useparams

---
+++ src/components/productPreview.jsx
@@ -0,0 +1,7 @@
+
+function ProductPreview(){
+    return(
+        <h1>Product Preview</h1>
+    )
+}
+export default ProductPreview;
--- src/pages/singleproduct.jsx
+++ src/pages/singleproduct.jsx
@@ -1,6 +1,14 @@
+import ProductPreview from "../components/productPreview"
+import { useParams } from "react-router-dom";
+
 function SingleProduct(){
+    const { id } = useParams()
     return(
-        <h1> Single Product</h1>
+        <>
+            <h1> Single Product {id} </h1>
+            <ProductPreview />
+        </>
+        
     );
 }
 export default SingleProduct;
